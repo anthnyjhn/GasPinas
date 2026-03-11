5ff7769680f0eed410a70af843d1a2b6ee83a713
anthnyjhn <johncaronongan17@gmail.com>
2026-03-11 22:09:25 +1100

    update header component and added short content in "about" page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <nav></nav>;
+  return (
+    <header className="w-full bg-blue-500 h-[100px]">
+      <h1 style={{ margin: 0, fontSize: "1.5rem", fontWeight: "normal" }}>
+        <a href="/" style={{ textDecoration: "none", color: "white" }}>
+          <b>GAS</b>PINAS
+        </a>
+      </h1>
+    </header>
+  );
 };
 
 export default Header;
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Check your gas prices near you and get the best possible deal.</p>
+    </div>
+  );
 };
 
 export default About;
